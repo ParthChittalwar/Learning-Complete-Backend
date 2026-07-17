fdb378de50e270a1916abc879a5f0fb35e9d1b53
Parth Santosh Chittalwar <chittalwarsantosh@gmail.com>
2026-07-18 02:24:28 +0530

    Project 1 Backend Integration Completed

---
+++ Day6/src/db/db.js
@@ -0,0 +1,11 @@
+const dns = require("node:dns");
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+const mongoose =  require("mongoose");
+
+const connectDB = async () => {
+
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Database Connected Successfully");
+}
+
+module.exports = connectDB;
